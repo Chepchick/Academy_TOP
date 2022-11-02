7215f070c9a503b68ee389ee8ead880166f13368
Aleksey_Suluyanov <suluyanov.aleksey@gmail.com>
2022-11-02 12:38:26 +0700

    HanoyTower

---
+++ IntroductionToOOP/Honoy_Tower/Main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+void HanoyTower(int number, int from, int to)
+{
+	if (number == 1)
+	{
+		cout << "Переместить диск №" << number << " со столбика " << from << " на столбик " << to << endl;
+	}
+	else {
+
+		int temp = 6 - from - to;
+		HanoyTower(number - 1, from, temp);
+		cout << "Переместить диск №" << number << " со столбика " << from << " на столбик " << to << endl;
+		HanoyTower(number - 1, temp, to);
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	HanoyTower(4, 1, 2);
+
+	return 0;
+}
+
