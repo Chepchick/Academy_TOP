475de64e2677a4f10db60bb9109f8c264c397e5a
Алексей Сулуянов <suluyanov.aleksey@gmail.com>
2022-12-16 17:38:15 +0700

    ДЗ от 26.11.22

--- C-Sharp_programming_language_basics/tasks_from_26.11.22/obj/Debug/net7.0/tasks_from_26.11.22.AssemblyInfo.cs
+++ C-Sharp_programming_language_basics/tasks_from_26.11.22/obj/Debug/net7.0/tasks_from_26.11.22.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("task_from_10.12.22")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("tasks_from_26.11.22")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("task_from_10.12.22")]
-[assembly: System.Reflection.AssemblyTitleAttribute("task_from_10.12.22")]
+[assembly: System.Reflection.AssemblyProductAttribute("tasks_from_26.11.22")]
+[assembly: System.Reflection.AssemblyTitleAttribute("tasks_from_26.11.22")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
