26127bec46d3a27e9074dfc77277f51b2a8f1d5c
Aleksey_Suluyanov <suluyanov.aleksey@gmail.com>
2023-02-07 13:47:57 +0700

    ДЗ_от_03.12.22

--- C-Sharp_programming_language_basics/tasks_from_03.12.22/Program.cs
+++ C-Sharp_programming_language_basics/tasks_from_03.12.22/Program.cs
@@ -1,4 +1,5 @@
 ﻿#define TASK_
+using static System.Console;
 
 #if TASK_1
 //Объявить одномерный(5 элементов) массив с именем A и двумерный массив (3 строки, 4 столбца) дроб -
@@ -150,6 +151,7 @@ for (int i = 0; i < arr.GetLength(0); i++)
 }
 Console.WriteLine($"Минимальное значение массива {min}, его координаты {min_row + 1} , {min_col + 1}");
 Console.WriteLine($"Максимальное значение массива {max}, его координаты {max_row + 1 } , {max_col + 1}");
+
 if (min_row > max_row) {
     swap(ref min_row, ref max_row);
     swap(ref min_col, ref max_col);
@@ -161,9 +163,9 @@ else if (min_row == max_row && min_col > max_col)
 }
 
 int sum = 0;
-for (int i = min_row; i < max_row; i++)
+for (int i = min_row; i <= max_row; i++)
 {
-    for (int j = min_col; j < max_col; j++)
+    for (int j = (i == min_row ? min_col : 0); j < (i == max_row ? max_col : arr.GetLength(1)); j++)
     {
         sum+= arr[i, j];
     }
@@ -179,22 +181,217 @@ void swap(ref int x, ref int y)
 #endif //TASK_2
 
 #if TASK_3
+//Пользователь вводит строку с клавиатуры. Необходимо зашифровать данную строку используя шифр Цезаря.
+Write("Введите строку для шифровки: ");
+string? message = ReadLine();
+char[] arr_mess = message.ToArray();
+
+Write("Введите ключ для шифрования: ");
+int? key = Convert.ToInt32(ReadLine());
+
+for (int i = 0; i < arr_mess.Length; i++)
+    arr_mess[i] = Convert.ToChar(Convert.ToInt32(arr_mess[i]) + key);
+
+Write("Зашифрованное сообщение: ");
+for (int i = 0; i < arr_mess.Length; i++) Write(arr_mess[i]);
+
+
+WriteLine();
+for (int i = 0; i < arr_mess.Length; i++)
+    arr_mess[i] = Convert.ToChar(Convert.ToInt32(arr_mess[i]) - key);
+
+Write("Разшифрованное сообщение: ");
+for (int i = 0; i < arr_mess.Length; i++) Write(arr_mess[i]);
 
 #endif //TASK_3
 
 #if TASK_4
+//Создайте приложение, которое производит операции над матрицами:
+//-Умножение матрицы на число;
+//-Сложение матриц;
+//-Произведение матриц.
+
+Write("Задайте разер матриц (не меньше 2): ");
+int size = Convert.ToInt32(ReadLine());
+
+int [,] matrixA = new int [size,size];
+int[,] matrixB = new int[size, size];
+
+Random rnd = new Random();
+
+for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+
+        matrixA[i, j] = rnd.Next(0, 9);
+        matrixB[i, j] = rnd.Next(0, 9);        
+    }   
+}
+
+for (int i = 0; i < size; i++) { 
+    for (int j = 0; j < size; j++) {
+        Write(matrixA[i,j]+ " "); 
+    }
+    WriteLine();
+}
+WriteLine();
+
+for (int i = 0; i < size; i++) { 
+    for (int j = 0; j < size; j++) {
+        Write(matrixB[i,j]+ " ");
+    }
+    WriteLine();
+}
+WriteLine();
+WriteLine("Умножение матрици на число");
+Write("Задайте число на которое необходимо умножить матрицу A: ");
+int value = Convert.ToInt32(ReadLine());
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        Write(matrixA[i, j] * value + " ");
+    }
+    WriteLine();
+}
+WriteLine();
+WriteLine("Сложение двух матриц");
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        Write(matrixA[i, j] + matrixB[i, j] + " ");
+    }
+    WriteLine();
+}
+WriteLine();
+
+WriteLine("Результат умножения двух матриц");
+int[,] matrixС = new int[size, size];
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        for (int k = 0; k < size; k++)
+        {
+            matrixС[i, j] += matrixA[i, k] * matrixB[k, j];
+        }
+    }
+}
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        Write(matrixС[i, j]+ " ");
+    }
+    WriteLine();
+}
 
 #endif //TASK_4
 
 #if TASK_5
+//Пользователь с клавиатуры вводит в строку арифметическое выражение. Приложение должно посчитать 
+//его результат. Необходимо поддерживать только две операции: + и –
+
+do
+{
+    Write("Введите арифметическое выражение в формате (a +/- b): ");
+    string? arithmetic_expression = ReadLine();
+    char[] arr_expression = arithmetic_expression.ToCharArray();
+
+    bool operator_presence = false;
+    int first_value_size = 0;
+    int secon_value_size = 0;
+
+    for (int i = 0; i < arr_expression.Length; i++)
+    {
+        if (arr_expression[i] == '+' || arr_expression[i] == '-')
+        {
+            operator_presence = true;
+            continue;
+        }
+        if (!operator_presence) first_value_size++;
+        if (operator_presence) secon_value_size++;
+    }
+
+    char[] first_value_arr = new char[first_value_size];
+    char[] secon_value_arr = new char[secon_value_size];   
+    operator_presence = false;
+
+    for (int i = 0, j = 0; i < arr_expression.Length; i++)
+    {
+        if (arr_expression[i] == '+' || arr_expression[i] == '-')
+        {
+            operator_presence = true;            
+            continue;
+        }
+        if (!operator_presence) first_value_arr[i] = arr_expression[i];
+        if (operator_presence)
+        {
+            secon_value_arr[j] = arr_expression[i];
+            j++;
+        }
+    }
+
+    int first_value = Convert.ToInt32(new String(first_value_arr));
+    int secon_value = Convert.ToInt32(new String(secon_value_arr));
+
+    if (arithmetic_expression.Contains('+'))
+    {
+        WriteLine($"{first_value} + {secon_value} = {first_value + secon_value}");
+    }
+    else
+    {
+        WriteLine($"{first_value} - {secon_value} = {first_value - secon_value}");
+    }
+    ReadLine();
+    Console.Clear(); 
+} while (true);
 
 #endif //TASK_5
 
 #if TASK_6
+//Пользователь с клавиатуры вводит некоторый текст. Приложение должно изменять регистр первой буквы 
+//каждого предложения на букву в верхнем регистре.
+
+//Write("Введите предложение: ");
+//string message = ReadLine();
+string message = "таким. образом.   заглавная.  строка повторяется.     трижды. а вторая.  она же заключительная";
+char[] message_arr = message.ToCharArray();
+
+message_arr[0] = Char.ToUpper(message_arr[0]);
+for (int i = 0; i < message_arr.Length; i++){
+    if (message_arr[i] == '.')
+    {
+        while (message_arr[++i] == ' ');
+        message_arr[i] = Char.ToUpper(message[i]);
+    }
+}
+string msg = new string(message_arr);
+WriteLine(msg);
 
 #endif //TASK_6
 
 #if TASK_7
+//Создайте приложение, проверяющее текст на недопустимые слова. Если недопустимое слово найдено, оно 
+//должно быть заменено на набор символов *. По итогам работы приложения необходимо показать статистику 
+//действий. 
+
+//Write("Введите текст: ");
+//string? text = ReadLine();
+string? text = "Я люблю море. Я лечу на море. Я умею плавать в море. Какое чистое море! Хочу на море. Завтра поедем на море. Акулы плавают в море";
+
+//Write("Введите не желательное слово: ");
+//string? word = ReadLine();
+string? word = "море";
+
+string replacement = new string('*', word.Length);
+
+text = text?.Replace(word, replacement);
+
+WriteLine(text);
 
 #endif //TASK_7
 
